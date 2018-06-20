ec78b3e6857da1a4608407a83e3555246b56b660
karol606 <karol606@hotmail.com>
2018-06-20 21:52:10 +0200

    Revert "Integracja z frontem"
    This reverts commit 5bf95912ca6730ded37e1ffd84b14a902f384cfb.
    
    # Conflicts:
    #	src/main/java/ASOserver/springapp/mapper/CustomerMapper.java

--- src/main/java/ASOserver/springapp/mapper/CustomerMapper.java
+++ src/main/java/ASOserver/springapp/mapper/CustomerMapper.java
@@ -16,11 +16,8 @@ public class CustomerMapper {
         customer.seteMail(customerDTO.geteMail());
         customer.setPesel(customerDTO.getPesel());
         customer.setPhoneNumber(customerDTO.getPhoneNumber());
-<<<<<<< HEAD
         customer.setAccount(new AccountMapper().toAccount(customerDTO.getAccountDTO()));
-=======
-        customer.setAccount(new AccountMapper().toAccount(customerDTO.getAccount()));
->>>>>>> parent of 5bf9591... Integracja z frontem
+        customer.setAccount(new AccountMapper().toAccount(customerDTO.getAccountDTO()));
         return customer;
     }
 
@@ -32,14 +29,8 @@ public class CustomerMapper {
         customerDTO.seteMail(customer.geteMail());
         customerDTO.setPesel(customer.getPesel());
         customerDTO.setPhoneNumber(customer.getPhoneNumber());
-<<<<<<< HEAD
         customerDTO.setAccountDTO(new AccountMapper().toAccountDTO(customer.getAccount()));
-=======
-        customerDTO.setAccount(new AccountMapper().toAccountDTO(customer.getAccount()));
-        customerDTO.setCars(customer.getCustomerCars().stream()
-                .map(tmpCustomerCar -> CarsMapper.toCarsDTO(tmpCustomerCar.getCars()))
-                .collect(Collectors.toList()));
->>>>>>> parent of 5bf9591... Integracja z frontem
+        customerDTO.setAccountDTO(new AccountMapper().toAccountDTO(customer.getAccount()));
         return customerDTO;
     }
 
