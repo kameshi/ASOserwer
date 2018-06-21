16dffb718f79bbc5b287544a7d2a7633ce10746a
karol606 <karol606@hotmail.com>
2018-06-21 09:23:07 +0200

    Spring

--- src/main/java/ASOserver/springapp/service/AccountService.java
+++ src/main/java/ASOserver/springapp/service/AccountService.java
@@ -33,6 +33,25 @@ public class AccountService {
         return this.accountDAO.save(AccountMapper.toAccount(accountDTO));
     }
 
+    public Account getAccountIdiCos(String login) throws Exception {
+        Iterable<Account> accountIterable = this.accountDAO.findAll();
+        Account account =new Account();
+        for(Account tmpAccount : accountIterable) {
+            if (tmpAccount.getLogin().equals(login)) {
+                account.setAccessRights(tmpAccount.getAccessRights());
+                account.setLogin(tmpAccount.getLogin());
+                if (!tmpAccount.getAccessRights().equals("klient")) {
+                    account.setAccountId(tmpAccount.getEmployee().getEmployeeId());
+                    break;
+                } else {
+                    account.setAccountId(tmpAccount.getCustomer().getCustomerId());
+                    break;
+                }
+            }
+        }
+        return account;
+    }
+
     public Account updateAccount(Long accountId, AccountDTO accountDTO) throws Exception {
         accountDTO.setId(accountId);
         accountDTO.setPassword(HashUtils.generateHash(accountDTO.getPassword(), 10));
