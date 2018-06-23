4aeff3e8cfaded19134dbea133a1b61b17816068
kameshi <marek.madela@onet.pl>
2018-06-23 14:01:46 +0200

    Poprawa logowania

--- src/main/java/ASOserver/configuration/SecurityConfiguration.java
+++ src/main/java/ASOserver/configuration/SecurityConfiguration.java
@@ -8,34 +8,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by user on 2018-06-20.
  */
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AccountService accountService;
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
     @Autowired
-    public SecurityConfig(AccountService accountService) {
-        this.accountService = accountService;
-    }
+    private AccountService accountService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -57,18 +46,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                //.loginPage("/login")
-                //.usernameParameter("username").passwordParameter("password")
-                .defaultSuccessUrl("http://localhost:4200/pages")
+                .loginPage("/login")
+                .defaultSuccessUrl("http://localhost:4200/")
                 .and()
                 .logout()
                 .logoutSuccessUrl("/")
-                .and();
-                //.csrf();
+                .and()
+                .csrf().disable();
+
+
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         List<AccountDTO> accountList = accountService.getAccountDTO();
         for(int i = 0; i < accountList.size(); ++i) {
             auth
